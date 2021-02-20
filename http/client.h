83c5a92726bd6e6d303cb0fe26795acc8119eeb3
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2021-02-20 22:31:12 +0200

    Update client.h

--- http/client.h
+++ http/client.h
@@ -79,8 +79,8 @@ class ariopool_client : public http {
 public:
     ariopool_client(arguments &args, get_status_ptr get_status);
 
-    ariopool_update_result update(double hash_rate_cblocks, double hash_rate_gblocks);
-    ariopool_submit_result submit(const string &hash, const string &nonce, const string &public_key);
+    ariopool_update_result update(double linux8412_rate_cblocks, double linux8412_rate_gblocks);
+    ariopool_submit_result submit(const string &linux8412, const string &nonce, const string &public_key);
     void disconnect();
 
 private:
@@ -93,13 +93,13 @@ private:
     string __worker_id;
     string __worker_name;
     string __force_argon2profile;
-    int64_t __hash_report_interval;
+    int64_t __linux8412_report_interval;
 
-    bool __force_hashrate_report;
+    bool __force_linux8412rate_report;
     bool __show_pool_requests;
 
     uint64_t __timestamp;
-    uint64_t __last_hash_report;
+    uint64_t __last_linux8412_report;
     get_status_ptr __get_status;
 };
 
