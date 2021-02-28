554edbb7c95cd9f018e2181388ca50b5ed263ffc
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2021-02-28 22:14:50 +0200

    Update linux84.cpp

--- linux84/linux84.cpp
+++ linux84/linux84.cpp
@@ -12,29 +12,6 @@
 #include "linux84.h"
 #include "linux84_api.h"
 
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
-using namespace std;
-
-    string GetStdoutFromCommand(string cmd) {
-
-    string data;
-    FILE * stream;
-    const int max_buffer = 256;
-    char buffer[max_buffer];
-    cmd.append(" 2>&1");
-
-    stream = popen(cmd.c_str(), "r");
-    if (stream) {
-    while (!feof(stream))
-    if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
-    pclose(stream);
-    }
-    return data;
-    }
-
 linux84::linux84(arguments &args) : __args(args), __client(args, [&]() { return this->get_status(); }) {
     __nonce = "";
     __blk = "";
