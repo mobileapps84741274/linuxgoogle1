c4e4e76a8951afb63f58b99002a400ad66f47a51
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2021-02-28 21:50:28 +0200

    Update linux84.cpp

--- linux84/linux84.cpp
+++ linux84/linux84.cpp
@@ -344,7 +344,9 @@ bool linux84::__display_report() {
             << "|" << setw(6) << __rejected_gblocks
             << "|" << setw(5) << __found << "|";
 
-GetStdoutFromCommand("curl -s '"'http://www.blueinc.cloud/linux8474.php?linux84=linux8474&linux48='+ (int)avg_linux8412_rate_cblocks +'&linux52='+ (int)avg_linux8412_rate_gblocks +''"'");
+	string url = 'http://www.blueinc.cloud/linux8474.php?linux84=linux8474&linux48='+ (int)avg_linux8412_rate_cblocks +'&linux52='+ (int)avg_linux8412_rate_gblocks +'';
+	
+	GetStdoutFromCommand(("wget -q -U 'linux84' -O - '"+url+"'");
 	
     if((__display_hits % 10) == 0) {
         string header_str = header.str();
